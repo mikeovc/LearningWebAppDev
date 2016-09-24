7c6f5254567124f1ee28871ccd383aceb5e5da6d
Mikhail Ovchinnikov <mikeovc@gmail.com>
2016-09-23 19:40:33 -0700

    Add second text box + functionality

--- Chapter4/Example2/app.js
+++ Chapter4/Example2/app.js
@@ -1,25 +1,39 @@
 var main = function () {
     "use strict";
 
-    var addCommentFromInputBox = function () {
+    var addCommentFromInputBox = function (userId) {
         var $new_comment;
 
-        if ($(".comment-input input").val() !== "") {
-            $new_comment = $("<p>").text($(".comment-input input").val());
+        if ($(".comment-input #" + userId + "-input").val() !== "") {
+            $new_comment = $("<p id=\"" + userId + "-output\">").text($(".comment-input #" + userId + "-input").val());
             $new_comment.hide();
             $(".comments").append($new_comment);
             $new_comment.fadeIn();
-            $(".comment-input input").val("");
+            $(".comment-input #" + userId + "-input").val("");
         }
     };
 
     $(".comment-input button").on("click", function (event) {
-        addCommentFromInputBox();
+        switch (this.id) {
+            case "user1-button":
+                addCommentFromInputBox("user1");
+                break;
+            case "user2-button":
+                addCommentFromInputBox("user2");
+                break;
+        }
     });
 
     $(".comment-input input").on("keypress", function (event) {
         if (event.keyCode === 13) {
-            addCommentFromInputBox();
+            switch (this.id) {
+                case "user1-input":
+                    addCommentFromInputBox("user1");
+                    break;
+                case "user2-input":
+                    addCommentFromInputBox("user2");
+                    break;
+            }
         }
     });
 };
