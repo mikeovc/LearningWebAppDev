dd5dc3547794466ddbe1a56559d2cbe8fb305b89
Mikhail Ovchinnikov <mikeovc@gmail.com>
2016-09-23 20:17:10 -0700

    Fix warnings

--- Chapter4/Example2/app.js
+++ Chapter4/Example2/app.js
@@ -1,37 +1,40 @@
 var main = function () {
-    "use strict";
+    'use strict';
 
     var addCommentFromInputBox = function (userId) {
-        var $new_comment;
+        var $newComment;
+        var inputText;
 
-        if ($(".comment-input #" + userId + "-input").val() !== "") {
-            $new_comment = $("<p id=\"" + userId + "-output\">").text($(".comment-input #" + userId + "-input").val());
-            $new_comment.hide();
-            $(".comments").append($new_comment);
-            $new_comment.fadeIn();
-            $(".comment-input #" + userId + "-input").val("");
+        if ($('.comment-input #' + userId + '-input').val() !== '') {
+            $newComment = $('<p id=\'' + userId + '-output\'>');
+            inputText = $('.comment-input #' + userId + '-input').val();
+            $newComment.text(inputText);
+            $newComment.hide();
+            $('.comments').append($newComment);
+            $newComment.fadeIn();
+            $('.comment-input #' + userId + '-input').val('');
         }
     };
 
-    $(".comment-input button").on("click", function (event) {
+    $('.comment-input button').on('click', function (event) {
         switch (this.id) {
-            case "user1-button":
-                addCommentFromInputBox("user1");
+            case 'user1-button':
+                addCommentFromInputBox('user1');
                 break;
-            case "user2-button":
-                addCommentFromInputBox("user2");
+            case 'user2-button':
+                addCommentFromInputBox('user2');
                 break;
         }
     });
 
-    $(".comment-input input").on("keypress", function (event) {
+    $('.comment-input input').on('keypress', function (event) {
         if (event.keyCode === 13) {
             switch (this.id) {
-                case "user1-input":
-                    addCommentFromInputBox("user1");
+                case 'user1-input':
+                    addCommentFromInputBox('user1');
                     break;
-                case "user2-input":
-                    addCommentFromInputBox("user2");
+                case 'user2-input':
+                    addCommentFromInputBox('user2');
                     break;
             }
         }
