01acd6ed7dda320a46dc0fb77e326ed64c1455b2
Mikhail Ovchinnikov <mikeovc@gmail.com>
2016-09-26 14:47:19 -0700

    Add tab switching functionality

--- Chapter4/Amazeriffic/javascripts/app.js
+++ Chapter4/Amazeriffic/javascripts/app.js
@@ -1,6 +1,25 @@
 var main = function() {
 	"use strict";
-	
+
+	var makeTabActive = function(tabNumber) {
+		$(".tabs span").removeClass("active");
+		$(".tabs a:nth-child(" + tabNumber + ") span").addClass("active");
+		$("main .content").empty();
+		false;
+	};
+
+	$(".tabs a:nth-child(1)").on("click", function() {
+		makeTabActive(1);
+		return false;
+	});
+	$(".tabs a:nth-child(2)").on("click", function() {
+		makeTabActive(2);
+		return false;
+	});
+	$(".tabs a:nth-child(3)").on("click", function() {
+		makeTabActive(3);
+		return false;
+	});
 };
 
 $(document).ready(main);
