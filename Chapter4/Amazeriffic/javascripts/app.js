31b542c82bcf9e6c83cf197eb0d14a5c885bfff0
Mikhail Ovchinnikov <mikeovc@gmail.com>
2016-09-29 19:20:36 -0700

    Add first two tab functionalities

--- Chapter4/Amazeriffic/javascripts/app.js
+++ Chapter4/Amazeriffic/javascripts/app.js
@@ -1,29 +1,42 @@
 var main = function() {
-	"use strict";
+    "use strict";
+    var toDos = ["Finish reading this book",
+                 "Take Gracie to the park",
+                 "Answer emails",
+                 "Prep for Monday's class",
+                 "Make up some new ToDos",
+                 "Get Groceries"];
 
-	var makeTabActive = function(tabNumber) {
-		$(".tabs span").removeClass("active");
-		$(".tabs a:nth-child(" + tabNumber + ") span").addClass("active");
-		$("main .content").empty();
-		false;
-	};
+    $(".tabs a span").toArray().forEach(function (element) {
+        $(element).on("click", function () {
+            var $element = $(element), $content, i;
 
-	for (var i = 1; i < 4; i++) {
+            $(".tabs span").removeClass("active");
+            $element.addClass("active");
+            $("main .content").empty();
 
-	}
+            if ($element.parent().is(":nth-child(1)")) {
+                $content = $("<ul>");
+                for (i = toDos.length - 1; i >= 0; i--) {
+                    $content.append($("<li>").text(toDos[i]));
+                }
+                $("main .content").append($content);
+            } else if ($element.parent().is(":nth-child(2)")){
+                $content = $("<ul>");
+                toDos.forEach(function (toDo) {
+                    $content.append($("<li>").text(toDo));
+                });
+                $("main .content").append($content);
+            } else if ($element.parent().is(":nth-child(3)")) {
+                console.log('blah');
+            }
+            return false;
+        })
+    });
 
-	$(".tabs a:nth-child(1)").on("click", function() {
-		makeTabActive(1);
-		return false;
-	});
-	$(".tabs a:nth-child(2)").on("click", function() {
-		makeTabActive(2);
-		return false;
-	});
-	$(".tabs a:nth-child(3)").on("click", function() {
-		makeTabActive(3);
-		return false;
-	});
+    $(".tabs a:first-child span").trigger("click");
 };
 
 $(document).ready(main);
+
+//TODO: refactor amazerrific for add.class function
