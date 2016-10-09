432319a361c7595eb65fd405e8a0a51c7526d6fe
Mikhail Ovchinnikov <mikeovc@gmail.com>
2016-10-09 16:33:36 -0700

    Added 'Tabs' tab functionality

--- Chapter5/Amazeriffic/javascripts/app.js
+++ Chapter5/Amazeriffic/javascripts/app.js
@@ -3,7 +3,7 @@ var main = function (toDoObjects) {
 
     var toDos = toDoObjects.map(function (toDo) {
         return toDo.description;
-    })
+    });
 
     $(".tabs a span").toArray().forEach(function (element) {
         $(element).on("click", function () {
@@ -25,7 +25,45 @@ var main = function (toDoObjects) {
                 });
                 $("main .content").append($content);
             } else if ($element.parent().is(":nth-child(3)")) {
-                
+                // Reorgnaize todos.json into a map that flips
+                // the key and value, with each object in the
+                // todo.tag holding its own descriptor
+                var tags = [];
+
+                toDoObjects.forEach(function (toDo) {
+                    toDo.tags.forEach(function (tag) {
+                        // Check to make sure it's not already in tags[]
+                        if (tags.indexOf(tag) === -1) {
+                            tags.push(tag);
+                        }
+                    });
+                });
+
+                var tagObjects = tags.map(function (tag) {
+                    var toDosWithTag = [];
+
+                    toDoObjects.forEach(function (toDo) {
+                        if (toDo.tags.indexOf(tag) !== -1) {
+                            toDosWithTag.push(toDo.description);
+                        }
+                    });
+
+                    return { "name": tag, "toDos": toDosWithTag };
+                });
+
+                tagObjects.forEach(function (tag) {
+                    var $tagName = $("<h3>").text(tag.name),
+                        $content = $("<ul>");
+
+
+                    tag.toDos.forEach(function (description) {
+                        var $li = $("<li>").text(description);
+                        $content.append($li);
+                    });
+
+                    $("main .content").append($tagName);
+                    $("main .content").append($content);
+                });
             } else if ($element.parent().is(":nth-child(4)")) {
                 $button = $("<button>").text("+");
                 $input = $("<input>");
@@ -39,7 +77,7 @@ var main = function (toDoObjects) {
             }
             $("main .content").append($content);
             return false;
-        })
+        });
     });
 
     $(".tabs a:first-child span").trigger("click");
